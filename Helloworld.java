c52266bab523917cfd5c01d02e30978ddd9f97bd
Dinakaran97 <89441714+Dinakaran97@users.noreply.github.com>
2022-10-18 14:45:40 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+package Edubridgeprograms;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+System.out.println(" I am Dinakaran");
+System.out.println("I am doing capgemini certification in Java full stack");
+System.out.println("My batch number is 20228106");
+
+	}
+
+}
